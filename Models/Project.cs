574182a9935b35ad61e2d8c7b11e7f1992777427
Alex <alexandre.d.paquette+github@gmail.com>
2025-08-25 16:21:55 -0300

    rename github link to link

--- Models/Project.cs
+++ Models/Project.cs
@@ -5,14 +5,14 @@ public class Project
     public string Title { get; set; }
     public string Description { get; set; }
     public List<string> TechStack { get; set; }
-    public string GitHubLink { get; set; }
+    public string Link { get; set; }
     public string ImageSource { get; set; }
-    public Project(string title, string desc, List<string> stack, string githubLink, string imageSource)
+    public Project(string title, string desc, List<string> stack, string link, string imageSource)
     {
         Title = title;
         Description = desc;
         TechStack = stack;
-        GitHubLink = githubLink;
+        Link = link;
         ImageSource = imageSource;
     }
 }
