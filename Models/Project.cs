bc26ddb3bc035bd4d7908b0a81fda6f15eb25979
Alex <alexandre.d.paquette+github@gmail.com>
2025-08-25 20:43:03 -0300

    Update project implementation to be consistent with degree class

--- Models/Project.cs
+++ Models/Project.cs
@@ -2,17 +2,9 @@ namespace Models;
 
 public class Project
 {
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public List<string> TechStack { get; set; }
-    public string Link { get; set; }
-    public string ImageSource { get; set; }
-    public Project(string title, string desc, List<string> stack, string link, string imageSource)
-    {
-        Title = title;
-        Description = desc;
-        TechStack = stack;
-        Link = link;
-        ImageSource = imageSource;
-    }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public List<string> TechStack { get; set; } = new();
+    public string? Link { get; set; }
+    public string? ImageSource { get; set; }
 }
