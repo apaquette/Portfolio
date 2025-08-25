3f1d40baaafff39e1dbf9794af284c4e1bdf7c6e
Alex <alexandre.d.paquette+github@gmail.com>
2025-08-25 17:33:55 -0300

    Make properties nullable to remove project warnings

--- Models/Degree.cs
+++ Models/Degree.cs
@@ -3,9 +3,9 @@ namespace Models;
 public class Degree
 {
 
-    public string Diploma { get; set; }
-    public string Institution { get; set; }
+    public string? Diploma { get; set; }
+    public string? Institution { get; set; }
     public DateTime GraduationDate { get; set; }
-    public string Logo { get; set; }
-    public string Website { get; set; }
+    public string? Logo { get; set; }
+    public string? Website { get; set; }
 }
