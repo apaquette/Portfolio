f87f0a3420352a1cc2f5bb4e140c7fd8604e61eb
Alex <alexandre.d.paquette+github@gmail.com>
2025-09-05 00:28:40 -0300

    Implement first unit test, passes

---
+++ tests/Portfolio.Test/DegreeTests.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+
+namespace Portfolio.Test;
+
+public class DegreeTests
+{
+    [Test]
+    public void GraduationDate_ShouldBeRequired()
+    {
+        Degree degree = new()
+        {
+            Diploma = "BSc",
+            Institution = "Test University",
+            GraduationDate = new DateOnly(2022, 6, 1)
+        };
+
+        Assert.That(degree.GraduationDate, Is.EqualTo(new DateOnly(2022, 6, 1)));
+    }
+}
