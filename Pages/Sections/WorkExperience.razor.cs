8aef62f2167d3f69e06b556c7ecdb65aab7c3077
Alex <alexandre.d.paquette+github@gmail.com>
2025-09-04 21:50:36 -0300

    Implement code-behind for WorkExperience

---
+++ Pages/Sections/WorkExperience.razor.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Components;
+using Models;
+using System.Text.Json;
+
+namespace Portfolio.Pages.Sections;
+
+public partial class WorkExperience : ComponentBase
+{
+    protected SortedSet<Experience> WorkExperiences = new();
+    [Inject]
+    private HttpClient? Http { get; set; }
+    protected override async Task OnInitializedAsync(){
+        string data = Http is not null ? await Http.GetStringAsync("data/workExperience.json") : "";
+        JsonSerializerOptions options = new() { PropertyNameCaseInsensitive = true };
+        WorkExperiences = JsonSerializer.Deserialize<SortedSet<Experience>>(data, options) ?? new();
+    }
+}
