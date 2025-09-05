f25b9c563989d63cea30aa5e8c1c37e92ea485d2
Alex <alexandre.d.paquette+github@gmail.com>
2025-09-04 21:38:20 -0300

    Implement code-behind for Education component

---
+++ Pages/Sections/Education.razor.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components;
+using Models;
+using System.Text.Json;
+
+namespace Portfolio.Pages.Sections;
+
+public partial class Education : ComponentBase
+{
+    protected SortedSet<Degree> Degrees = new();
+    [Inject]
+    private HttpClient? Http { get; set; }
+    protected override async Task OnInitializedAsync(){
+        string data = Http is not null ? await Http.GetStringAsync("data/degrees.json") : "";
+        Degrees = JsonSerializer.Deserialize<SortedSet<Degree>>(data, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true }) ?? new();
+    }
+}
