55aef580929dae90ec0a02cf6e71212a9a128c46
Alex <alexandre.d.paquette+github@gmail.com>
2025-09-05 00:31:44 -0300

    Implement dates are equal unit test, passes

--- tests/Portfolio.Test/DegreeTests.cs
+++ tests/Portfolio.Test/DegreeTests.cs
@@ -17,11 +17,20 @@ public class DegreeTests
         Assert.That(degree.GraduationDate, Is.EqualTo(new DateOnly(2022, 6, 1)));
     }
     [Test]
-    public void CompareTo_WhenThisGraduationIsMoreRecent_ReturnsBeforeOther()
+    public void GraduationIsMoreRecent_ReturnsBeforeOther()
     {
         Degree newer = new() { GraduationDate = new DateOnly(2023, 6, 1) };
         Degree older = new() { GraduationDate = new DateOnly(2020, 6, 1) };
 
         Assert.That(newer.CompareTo(older), Is.LessThan(0)); // newer first
+    }
+
+    [Test]
+    public void GraduationDatesAreEqual_ReturnsZero()
+    {
+        Degree d1 = new() { GraduationDate = new DateOnly(2022, 6, 1) };
+        Degree d2 = new() { GraduationDate = new DateOnly(2022, 6, 1) };
+
+        Assert.That(d1.CompareTo(d2), Is.EqualTo(0));
     }
 }
