f6f5aca5b93f983d353836283a761e7bf780453a
Alex <alexandre.d.paquette+github@gmail.com>
2025-09-05 00:30:01 -0300

    Implement newer/older degree comparison test, passes

--- tests/Portfolio.Test/DegreeTests.cs
+++ tests/Portfolio.Test/DegreeTests.cs
@@ -16,4 +16,12 @@ public class DegreeTests
 
         Assert.That(degree.GraduationDate, Is.EqualTo(new DateOnly(2022, 6, 1)));
     }
+    [Test]
+    public void CompareTo_WhenThisGraduationIsMoreRecent_ReturnsBeforeOther()
+    {
+        Degree newer = new() { GraduationDate = new DateOnly(2023, 6, 1) };
+        Degree older = new() { GraduationDate = new DateOnly(2020, 6, 1) };
+
+        Assert.That(newer.CompareTo(older), Is.LessThan(0)); // newer first
+    }
 }
