d1a4b3128284e5b849d6004b8a3d4230427fa3b2
Alex <alexandre.d.paquette+github@gmail.com>
2025-09-05 22:44:38 -0300

    Implement multiple unit tests, fails most recent one

--- tests/Portfolio.Shared.Test/ExperienceTests.cs
+++ tests/Portfolio.Shared.Test/ExperienceTests.cs
@@ -7,7 +7,7 @@ public class ExperienceTests
     [Test]
     public void Skills_ShouldInitializeAsEmptyList()
     {
-        var exp = new Experience { JobStart = DateTime.Today };
+        var exp = new Experience(DateTime.Today) { JobStart = DateTime.Today };
         Assert.That(exp.Skills, Is.Not.Null);
         Assert.That(exp.Skills, Is.Empty);
     }
@@ -15,7 +15,7 @@ public class ExperienceTests
     [Test]
     public void Skills_WhenAdded_ShouldBeSortedAlphabetically()
     {
-        var exp = new Experience { JobStart = DateTime.Today, Skills = new() { "Zebra", "Apple", "Monkey" } };
+        var exp = new Experience(DateTime.Today) { JobStart = DateTime.Today, Skills = new() { "Zebra", "Apple", "Monkey" } };
 
         var sorted = exp.Skills.ToList();
         Assert.That(sorted, Is.EqualTo(new[] { "Apple", "Monkey", "Zebra" }));
@@ -24,8 +24,8 @@ public class ExperienceTests
     [Test]
     public void CompareTo_WhenThisJobEndMoreRecent_ReturnsBeforeOther()
     {
-        var older = new Experience { JobStart = new DateTime(2019, 1, 1), JobEnd = new DateTime(2020, 1, 1) };
-        var newer = new Experience { JobStart = new DateTime(2020, 1, 1), JobEnd = new DateTime(2023, 1, 1) };
+        var older = new Experience(new DateTime(2019,1,1)) { JobStart = new DateTime(2019, 1, 1), JobEnd = new DateTime(2020, 1, 1) };
+        var newer = new Experience(new DateTime(2020,1,1)) { JobStart = new DateTime(2020, 1, 1), JobEnd = new DateTime(2023, 1, 1) };
 
         Assert.That(newer.CompareTo(older), Is.LessThan(0));
     }
@@ -33,8 +33,8 @@ public class ExperienceTests
     [Test]
     public void CompareTo_WhenJobEndIsNull_TreatedAsOngoing()
     {
-        var past = new Experience { JobStart = new DateTime(2019, 1, 1), JobEnd = new DateTime(2020, 1, 1) };
-        var ongoing = new Experience { JobStart = new DateTime(2021, 1, 1), JobEnd = null };
+        var past = new Experience(new DateTime(2019,1,1)) { JobStart = new DateTime(2019, 1, 1), JobEnd = new DateTime(2020, 1, 1) };
+        var ongoing = new Experience(new DateTime(2021,1,1)) { JobStart = new DateTime(2021, 1, 1), JobEnd = null };
 
         Assert.That(ongoing.CompareTo(past), Is.LessThan(0));
     }
@@ -42,8 +42,8 @@ public class ExperienceTests
     [Test]
     public void CompareTo_BothNullJobEnd_ShouldBeEqual()
     {
-        var e1 = new Experience { JobStart = new DateTime(2020, 1, 1), JobEnd = null };
-        var e2 = new Experience { JobStart = new DateTime(2019, 1, 1), JobEnd = null };
+        var e1 = new Experience(new DateTime(2020, 1, 1)) { JobStart = new DateTime(2020, 1, 1), JobEnd = null };
+        var e2 = new Experience(new DateTime(2019, 1, 1)) { JobStart = new DateTime(2019, 1, 1), JobEnd = null };
 
         Assert.That(e1.CompareTo(e2), Is.EqualTo(0));
     }
@@ -55,12 +55,29 @@ public class ExperienceTests
     [TestCase("2021-01-01", "2021-06-01", "5 mos")]
     public void Duration_ShouldReturnExpectedFormat(string start, string end, string expected)
     {
-        var e = new Experience
+        var e = new Experience(DateTime.Parse(start))
         {
             JobStart = DateTime.Parse(start),
             JobEnd = DateTime.Parse(end)
         };
 
         Assert.That(e.Duration(), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Duration_NullJobEnd_UsesToday()
+    {
+        var e = new Experience(DateTime.Today.AddYears(-1)) { JobStart = DateTime.Today.AddYears(-1), JobEnd = null };
+
+        var result = e.Duration();
+
+        Assert.That(result, Does.Contain("1 yr"));
+    }
+
+    [Test]
+    public void JobStart_Required()
+    {
+        Experience exp = new(new DateTime(2022, 6, 1));
+        Assert.That(exp.JobStart, Is.EqualTo(new DateTime(2022, 6, 1)));
     }
 }
