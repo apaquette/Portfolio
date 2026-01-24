2967b1af46a0084b51cf98fe0828a88fa951c86b
Alex Paquette <alexandre.d.paquette@gmail.com>
2026-01-24 21:18:59 +0000

    Implement certification model

---
+++ src/Portfolio.Shared/Models/Certification.cs
@@ -0,0 +1,16 @@
+namespace Models;
+
+public class Certification : IComparable<Certification>
+{
+    public string Name { get; set; }
+    public DateOnly EarnedOn { get; set; }
+    public string IssuedBy { get; set; }
+    public string Icon { get; set; }
+    public string Link { get; set; }
+
+    public int CompareTo(Certification? other)
+    {
+        if (other is null) return 1;
+        return other.EarnedOn.CompareTo(EarnedOn);
+    }
+}
