e322a9154543bb49e8964fac88deef32887c0c3f
Alex Paquette <alexandre.d.paquette@gmail.com>
2026-05-03 23:21:26 -0300

    Update CardComponent.razor
    Add support for potional Icon at the top, and wrap child-content in card-text paragraph. Also add tags at the bottom

--- src/Portfolio/Pages/Components/CardComponent.razor.cs
+++ src/Portfolio/Pages/Components/CardComponent.razor.cs
@@ -8,7 +8,13 @@ public partial class CardComponent : ComponentBase
     [Parameter][Required]
     public string Title { get; set; } = "";
     [Parameter]
+    public string? Link { get; set; }
+    [Parameter]
+    public RenderFragment? Icon { get; set; }
+    [Parameter]
     public string? Subtitle { get; set; }
     [Parameter]
     public RenderFragment? ChildContent { get; set; }
+    [Parameter]
+    public string[]? Tags { get; set; }
 }
