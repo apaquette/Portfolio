b00523e3c3d98a855bd8b13bcb6e6a06d69c4638
Alex Paquette <alexandre.d.paquette@gmail.com>
2026-05-10 12:00:49 -0300

    Simplify navigation

--- src/Portfolio/Program.cs
+++ src/Portfolio/Program.cs
@@ -2,7 +2,6 @@ using System.Text.Json;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Portfolio;
-using Portfolio.Navigation;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,6 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton(new JsonSerializerOptions {PropertyNameCaseInsensitive = true});
-builder.Services.AddSingleton<INavigationService, NavigationService>();
 
 await builder.Build().RunAsync();
